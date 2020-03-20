6cfc16176b1f714c0aacf0f42889194f043aa73e
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-20 12:05:01 +0100

    @Gerglamesh

---
+++ Source/SpacePort/API/API-Caller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using SpacePort.API;
+
+namespace SpacePort
+{
+    public class API_Caller
+    {
+        private static RestClient client =  new RestClient("https://swapi.co/api/");
+        
+        public CharacterInformationRoot GetCharacter(string search = "")
+        {
+            var request = new RestRequest("people/", DataFormat.Json);
+            var response = client.Get<CharacterInformationRoot>(request);
+            
+            return response.Data;
+        }
+        public SpaceshipInformationRoot GetSpaceship(string search = "")
+        {
+            var request = new RestRequest("starships/", DataFormat.Json);
+            var response = client.Get<SpaceshipInformationRoot>(request);
+
+            return response.Data;
+        }
+
+    }
+    
+}
+
---
+++ Source/SpacePort/API/SpaceshipInformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpacePort.API
+{
+    public class SpaceshipInformation
+    {
+        public string name { get; set; }
+        public string model { get; set; }
+        public string manufacturer { get; set; }
+        public string cost_in_credits { get; set; }
+        public string length { get; set; }
+        public string max_atmosphering_speed { get; set; }
+        public string crew { get; set; }
+        public string passengers { get; set; }
+        public string cargo_capacity { get; set; }
+        public string consumables { get; set; }
+        public string hyperdrive_rating { get; set; }
+        public string MGLT { get; set; }
+        public string starship_class { get; set; }
+        public List<object> pilots { get; set; }
+        public List<string> films { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+
+    }
+
+    public class SpaceshipInformationRoot
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<SpaceshipInformation> results { get; set; }
+    }
+
+
+}
