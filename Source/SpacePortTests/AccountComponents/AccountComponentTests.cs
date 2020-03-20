e5d4dc64c20a98d8c7d79a60c274887e3e95bf5a
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-03-20 14:30:55 +0100

    @MirkoPralica

--- Source/SpacePortTests/AccountComponents/AccountComponentTests.cs
+++ Source/SpacePortTests/AccountComponents/AccountComponentTests.cs
@@ -9,15 +9,58 @@ namespace SpacePort.Tests
     [TestClass()]
     public class AccountComponentTests
     {
-        
+
         [TestMethod()]
-        public void WithdrawTest()
+        public void WithdrawTest_WithdrawNegativeNumber_ReturnZero()
         {
             double amount = 100;
             AccountComponent account = new AccountComponent(amount);
-            account.Withdraw(-100);
+            double result = account.Withdraw(-100);
 
-            Assert.AreEqual(100, account.CheckBalance());
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void WithdrawTest_WithdrawTooMuch_ReturnZero()
+        {
+            double amount = 100;
+            AccountComponent account = new AccountComponent(amount);
+            double result = account.Withdraw(1000);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void WithdrawTest_WithdrawLessThanBalance_ReturnAmount()
+        {
+            double amount = 100;
+            AccountComponent account = new AccountComponent(amount);
+            double result = account.Withdraw(99);
+
+            Assert.AreEqual(99, result);
+        }
+
+        [TestMethod()]
+        public void WithdrawTest_WithdrawAll_ReturnAll()
+        {
+            double amount = 100;
+            AccountComponent account = new AccountComponent(amount);
+            double result = account.Withdraw(100);
+
+            Assert.AreEqual(100, result);
+            Assert.AreEqual(0, account.CheckBalance());
+        }
+
+        [TestMethod()]
+        public void DepositTest_DepositNegativeNumber_BalanceUnchange()
+        {
+            double depositAmount = -100;
+            double amount = 100;
+            AccountComponent account = new AccountComponent(amount);
+            account.Deposit(depositAmount);
+
+            Assert.AreEqual(amount - depositAmount, account.CheckBalance());
+            //Assert.AreEqual(0, account.CheckBalance());
         }
     }
 }
