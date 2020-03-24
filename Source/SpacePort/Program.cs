d9c26d74b30638b98fe60422ce9ebafc5f72be96
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-24 14:38:00 +0100

    @Gerglamesh, confirmed that API returns only one object on exact searches based on ship names.

--- Source/SpacePort/Program.cs
+++ Source/SpacePort/Program.cs
@@ -12,24 +12,11 @@ namespace SpacePort
         static void Main(string[] args)
         {
             System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-            List<SpaceShip> spaceShips = new List<SpaceShip>();
-            List<Person> characters = new List<Person>();
-            ApiFetchData data = new ApiFetchData();
-            SpaceParkCorp s = new SpaceParkCorp();
 
             SpaceShip falcon = SpaceShipObjectBuilder.BuildFromApi("destroyer");
-            SpaceShip falcon2 = SpaceShipObjectBuilder.BuildFromApi(9);
-            SpaceShip falcon3 = SpaceShipObjectBuilder.BuildFromApi(10);
-            SpaceShip falcon4 = SpaceShipObjectBuilder.BuildFromApi(9);
-
             falcon.AddPassenger(PersonObjectBuilder.BuildFromApi(1));
-
             Console.WriteLine(falcon.PassengerList()[0].Name);
 
-            
-            ApiFetchData apiFetch = new ApiFetchData();
-
-            Console.ReadKey();
         }
     }
 }
